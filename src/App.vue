e57d480879877e7a7f539580805f9f9f955eebe4
JD Solanki <jdsolanki0001@gmail.com>
2022-04-20 10:31:39 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
+    <v-main class="pa-6">
+      <v-card title="Button">
+        <v-card-content class="d-flex" style="gap: 1.5rem;">
+          <v-btn color="primary">Primary</v-btn>
+          <v-btn color="success">Success</v-btn>
+          <v-btn color="warning">Warning</v-btn>
+          <v-btn color="error">Error</v-btn>
+        </v-card-content>
+      </v-card>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
 }
 </script>
+
+<style lang="scss">
+.v-application {
+  background-color: #f1f1f1 !important;
+}
+</style>
